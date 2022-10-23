aa2edc5cd4fa2d099b3bfb1ac2dbdd1f670ac810
lucaramseyer <112868331+lucaramseyer@users.noreply.github.com>
2022-10-23 14:19:59 +0200

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -134,3 +134,31 @@ for epoch in range(num_epochs):
 print('Finished Training')
 PATH = './cnn.pth'
 torch.save(model.state_dict(), PATH)
+
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(11)]
+    n_class_samples = [0 for i in range(11)]
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images.float())
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+        for i in range(labels.size(0)):
+            label = labels[i]
+            pred = predicted[i]
+            if (label == pred):
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network: {acc} %')
+
+    for i in range(11):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'Accuracy of {classes[i]}: {acc} %')
